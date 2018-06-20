cf156bf6998c0942ac5967df2f963e66c61b21ce
Akash Sunil Gaikwad <akashsunilgaikwad@gmail.com>
2018-06-20 03:02:12 -0400

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -13,12 +13,13 @@ def bubble_sort(arr):
     number_of_elements =len(arr)
     for k in range(number_of_elements-1):
         for i in range(number_of_elements-1-k):
-            temp = arr[i]
-            arr[i] = arr[i+1]
-            arr[i+1] = temp
+            if(arr[i]>arr[i+1]):
+                temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
     return arr
 
-arr =[5, 4, 3, 2, 1, 0]
+arr =[5, 4, 2,3, 1, 0]
 
 new_arr = bubble_sort(arr)
 print(new_arr)
